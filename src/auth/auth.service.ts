23816c2f255466d37542e228c6ef6811ac07a66b
Tunahan BUCAK <int.tunahan.bucak@aurorabilisim.com>
2024-07-30 08:31:06 +0300

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async signupLocal(dto: AuthDto): Promise<any> {
+    //Promise<Tokens>
+    const hash = await this.hashData(dto.password);
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+  }
+  signinLocal() {}
+  logout() {}
+  refreshTokens() {}
+}
---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrkey: 'at-secret',
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
